07d7286887817da77ca063d69cdcbaaa5043cfc2
mixool <mixool0204@gmail.com>
2020-12-15 12:14:26 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 # config
 [[ "$MY_PREF_INI" != "" ]] && wget -qO- $MY_PREF_INI | sed "s/port=[0-9]+$/port=$PORT/g" >/subconverter/pref.ini
 sed -i -e "s/port=25500/port=$PORT/g" -e "s/api_access_token=password/api_access_token=$API_TOKEN/g" /subconverter/pref.ini
-wget -O /subconverter/profiles/pro.ini $MY_PRO_INI
+wget --no-check-certificate --no-cache --no-cookies -O /subconverter/profiles/pro.ini $MY_PRO_INI
 
 # start
 /subconverter/subconverter
