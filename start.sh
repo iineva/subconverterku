d9f3a1ac78e8e2b647a2e8729761871823f3036e
Steven <s@ineva.cn>
2021-04-23 01:26:28 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@
 [[ "$MY_PREF_INI" != "" ]] && wget -qO- $MY_PREF_INI | sed "s/port=[0-9]+$/port=$PORT/g" >/subconverter/pref.ini
 sed -i -e "s/port=25500/port=$PORT/g" -e "s/api_access_token=password/api_access_token=$API_TOKEN/g" /subconverter/pref.ini
 sed -i -e "s/update_ruleset_on_request=false/update_ruleset_on_request=true/g" /subconverter/pref.ini
-sed -i -e "s/managed_config_prefix=http:\/\/127.0.0.1:25500/managed_config_prefix=https:\/\/ineva-sub.herokuapp.com/g" /subconverter/pref.ini
+sed -i -e "s/managed_config_prefix=http:\/\/127.0.0.1:25500/managed_config_prefix=$(PUBLIC_URL)/g" /subconverter/pref.ini
 
 wget --no-check-certificate --no-cache --no-cookies -O /subconverter/profiles/pro.ini $MY_PRO_INI
 
